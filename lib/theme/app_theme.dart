be27fb0496d2bd5cd9d0208e5e1c870694ee377c
jermp96 <jjeremyperez96@gmail.com>
2023-06-12 12:30:08 -0600

    feat(ap_theme): centerTitle false on appBarTheme settings

--- lib/theme/app_theme.dart
+++ lib/theme/app_theme.dart
@@ -23,7 +23,10 @@ class AppTheme {
 
   ThemeData getTheme() => ThemeData(
     useMaterial3: true,
-    colorSchemeSeed: colorList[selectedColor]
+    colorSchemeSeed: colorList[selectedColor],
+    appBarTheme: const AppBarTheme(
+      centerTitle: false
+    )
   );
 
 }
