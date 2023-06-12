76f018b0ae77bddba29ac061909c5238e482bcdc
jermp96 <jjeremyperez96@gmail.com>
2023-06-12 12:31:28 -0600

    feat(menu_items): *MenuItem model added *menu items list added

---
+++ lib/menu/menu_items.dart
@@ -0,0 +1,30 @@
+import 'package:flutter/material.dart';
+
+class MenuItem {
+  final IconData icon;
+  final String link;
+  final String subTitle;
+  final String title;
+
+  const MenuItem({
+    required this.icon,
+    required this.link,
+    required this.subTitle,
+    required this.title
+  });
+}
+
+const appMenuItems = <MenuItem> {
+  MenuItem(
+    icon: Icons.smart_button_outlined,
+    link: '/buttons',
+    subTitle: 'Varios botones',
+    title: 'Botones'
+  ),
+    MenuItem(
+    icon: Icons.credit_card,
+    link: '/card',
+    subTitle: 'Un contenedor estilizado',
+    title: 'Tarjetas'
+  ),
+};
