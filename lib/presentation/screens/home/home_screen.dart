810f645b7722dda84456cfd56d5bad9bf2c87410
jermp96 <jjeremyperez96@gmail.com>
2023-06-13 21:05:57 -0600

    *routesNamed implemented

--- lib/presentation/screens/home/home_screen.dart
+++ lib/presentation/screens/home/home_screen.dart
@@ -1,8 +1,11 @@
 import 'package:flutter/material.dart';
 import 'package:go_router/go_router.dart';
 import 'package:widgets_app/config/menu/menu_items.dart';
+import 'package:widgets_app/presentation/screens/screens.dart';
 
 class HomeScreen extends StatelessWidget {
+  static const String name = 'home_screen';
+
   const HomeScreen({Key? key}) : super(key: key);
 
   @override
@@ -51,11 +54,15 @@ class _CustomListTile extends StatelessWidget {
       subtitle: Text(menuItem.subTitle),
       onTap: () {
         // TODO: navigate to other page
+        // Flutter
         // Navigator.of(context).push(
         //   MaterialPageRoute(
         //     builder: (context) => const ButtonsScreen(),
         //   )
         // );
+
+        // Go Router
+        // context.pushNamed(CardsScreen.name);
         context.push(menuItem.link);
       },
     );
