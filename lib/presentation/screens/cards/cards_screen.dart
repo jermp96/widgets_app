170b0dd137f34403ee90823a16ebbd2d80d93565
jermp96 <jjeremyperez96@gmail.com>
2023-12-24 13:11:27 -0600

    feat: *add cards with images

--- lib/presentation/screens/cards/cards_screen.dart
+++ lib/presentation/screens/cards/cards_screen.dart
@@ -40,7 +40,17 @@ class _CardsView extends StatelessWidget {
 
           ...cards.map(
             (card) => _CardType2(elevation: card['elevation'], label: card['label'],)
-          )
+          ),
+
+          ...cards.map(
+            (card) => _CardType3(elevation: card['elevation'], label: card['label'],)
+          ),
+
+          ...cards.map(
+            (card) => _CardType4(elevation: card['elevation'], label: card['label'],)
+          ),
+
+          const SizedBox(height: 50,)
         ],
       ),
     );
@@ -128,5 +138,86 @@ class _CardType2 extends StatelessWidget {
   }
 }
 
+class _CardType3 extends StatelessWidget {
 
+  final String label;
+  final double elevation;
 
+  const _CardType3({
+    required this.label,
+    required this.elevation
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    final colors = Theme.of(context).colorScheme;
+
+    return Card(
+      color: colors.surfaceVariant,
+      elevation: elevation,
+      child: Padding(
+        padding: const EdgeInsets.fromLTRB(10, 5, 10, 10),
+        child: Column(
+          children: [
+            Align(
+              alignment: Alignment.topRight,
+              child: IconButton(
+                icon: const Icon(Icons.more_vert_outlined),
+                onPressed: () {},
+              )
+            ),
+            Align(
+              alignment: Alignment.bottomLeft,
+              child: Text('$label - Filled'),
+            )
+          ],
+        ),
+      ),
+    );
+  }
+}
+
+class _CardType4 extends StatelessWidget {
+
+  final String label;
+  final double elevation;
+
+  const _CardType4({
+    required this.label,
+    required this.elevation
+  });
+
+  @override
+  Widget build(BuildContext context) {
+
+    return Card(
+      clipBehavior: Clip.hardEdge,
+      elevation: elevation,
+      child: Stack(
+        children: [
+          Image.network(
+            'https://picsum.photos/id/${elevation.toInt()}/600/350',
+            height: 350,
+            fit: BoxFit.cover,
+          ),
+      
+          Align(
+            alignment: Alignment.topRight,
+            child: Container(
+              decoration: BoxDecoration(
+                color: Colors.white,
+                borderRadius: BorderRadius.only(
+                  bottomLeft: Radius.circular(20)
+                )
+              ),
+              child: IconButton(
+                icon: const Icon(Icons.more_vert_outlined),
+                onPressed: () {},
+              ),
+            )
+          )
+        ],
+      ),
+    );
+  }
+}
